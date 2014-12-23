f9ff20b1357db1efb232854820b4fee9e0f8aeaa
Anton <tonan91@gmail.com>
2014-12-23 21:38:53 +0100

    la till lite i calendarQueryBean

--- 2DV012/src/queryBeans/CalendarQueryBean.java
+++ 2DV012/src/queryBeans/CalendarQueryBean.java
@@ -2,6 +2,8 @@ package queryBeans;
 
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 /**
  * Session Bean implementation class CalendarQueryBean
@@ -9,6 +11,11 @@ import javax.ejb.Stateless;
 @Stateless
 @LocalBean
 public class CalendarQueryBean {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@PersistenceContext(name = "2DV012")
+	EntityManager em;
 
     /**
      * Default constructor. 
